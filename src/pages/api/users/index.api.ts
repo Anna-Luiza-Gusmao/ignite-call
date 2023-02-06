92514b540dec5de116529cf166d83e888d9f3df4
anna-luiza-haus <luizasgusmao@hotmail.com>
2023-02-06 10:56:41 -0300

    AddSingleUserValidation

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -7,6 +7,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     const { name, username } = req.body
+
+    const userExists = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    })
+
+    if(userExists) {
+        return res.status(400).json({
+            message: 'Username already taken.'
+        })
+    }
+
     const user = await prisma.user.create({
         data: {
             name,
